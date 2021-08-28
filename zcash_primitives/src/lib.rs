229e5bda86aa2ff9bb943f2a65cdf469de262536
jesse <53130380+Mr-Bossman@users.noreply.github.com>
2021-08-28 12:11:50 -0400

    Update lib.rs

--- zcash_primitives/src/lib.rs
+++ zcash_primitives/src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! `zcash_primitives` is a library that provides the core structs and functions necessary
 //! for working with Zcash.
-
+#![feature(restricted_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 // Catch documentation errors caused by code changes.
 #![deny(broken_intra_doc_links)]
